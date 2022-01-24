4eedf598a9872d26aee383bef0b224aa5069b881
Artem Batyr <artembatyr88@gmail.com>
2022-01-24 14:54:03 +0200

    react app posts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useDebugValue, useEffect, useState} from "react";
 import "./styles/App.css"
 import {BrowserRouter as Router, Route, Link, Routes, Switch, Redirect, Navigate} from "react-router-dom";
 import About from "./pages/About";
@@ -6,15 +6,33 @@ import Posts from "./pages/Posts";
 import Navbar from "./components/UI/Navbar/Navbar";
 import Error from "./pages/Error";
 import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if(localStorage.getItem('auth')) {
+            setIsAuth(true)
+        }
+        setLoading(false)
+    }, [])
+
     return (
-        <Router>
-            <Fragment>
-            <Navbar/>
-                <AppRouter/>
-            </Fragment>
-        </Router>
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <Router>
+                <Fragment>
+                    <Navbar/>
+                    <AppRouter/>
+                </Fragment>
+            </Router>
+        </AuthContext.Provider>
+
     )
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
 import {privateRoutes, publicRoutes} from "../router/routes";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+    if(isLoading) {
+        return <Loader/>
+    }
     return (
-        <Routes>
-            {privateRoutes.map(route =>
-                <Route
-                    exact={route.exact}
-                    path={route.path}
-                    element={<route.element/>}
-                />
-            )}
-            {publicRoutes.map(route =>
-                <Route
-                    exact={route.exact}
-                    path={route.path}
-                    element={<route.element/>}
-                />
-            )}
-            <Route path='*' element={<Navigate to="/posts"/>}/>
-        </Routes>
+        isAuth
+            ?
+            <Routes>
+                {privateRoutes.map(route =>
+                    <Route
+                        exact={route.exact}
+                        path={route.path}
+                        element={<route.element/>}
+                        key={route.path}
+                    />
+                )}
+                <Route path='*' element={<Navigate to="/posts"/>}/>
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map(route =>
+                    <Route
+                        exact={route.exact}
+                        path={route.path}
+                        element={<route.element/>}
+                        key={route.path}
+                    />
+                )}
+                <Route path='*' element={<Navigate to="/login"/>}/>
+            </Routes>
     );
 };
 
